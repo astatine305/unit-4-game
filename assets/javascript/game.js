d3c4e58a330c63ca76b3b3fa14e7423c7c70dac7
Mr. Chow <37449389+astatine305@users.noreply.github.com>
2018-05-13 17:06:57 -0700

    update

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+
+var yourNumber = 0;
+var randNumberBox = randNumber();
+var wins = 0;
+var losses = 0;
+var crystals;
+
+//random number between 1 and 200
+function randNumber() {
+    return Math.floor(Math.random() * 200) + 1;
+  }
+
+
+function resetGame() {
+$("#wrapper").empty();
+}
+
+
+
+
+
+
+})
